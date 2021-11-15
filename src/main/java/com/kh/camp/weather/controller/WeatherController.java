6e5f7d92d87dca10ced8677296ec9aa77ccc4b60
SunghyunOH <graceoh89@gmail.com>
2021-11-15 16:46:20 +0900

    1115_16:46commit

--- src/main/java/com/kh/camp/weather/controller/WeatherController.java
+++ src/main/java/com/kh/camp/weather/controller/WeatherController.java
@@ -1,6 +1,16 @@
 package com.kh.camp.weather.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class WeatherController {
 
-	//test.......plz....
+	
+	@RequestMapping("weather/weatherView.do")
+	public String weatherView() {
+		
+		return "weather/weather";
+	}
+	
 }
